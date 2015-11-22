2c91db597c79e39855e1fec430a2472087b2362d
Xiulan Shi <xiulanshi2013@gmail.com>
2015-11-21 19:25:16 -0500

    First commit

---
+++ Jump/GameScene.swift
@@ -0,0 +1,47 @@
+//
+//  GameScene.swift
+//  Jump
+//
+//  Created by Xiulan Shi on 11/21/15.
+//  Copyright (c) 2015 Xiulan Shi. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    
+//    // Second Step: This is the blank canvas onto which you’ll add your game nodes.
+//    required init?(coder aDecoder: NSCoder) {
+//        super.init(coder: aDecoder)
+//    }
+//    
+//    override init(size: CGSize) {
+//        super.init(size: size)
+//        backgroundColor = SKColor.whiteColor()  
+//    }
+//    
+//    // First add the background node
+//    
+//    func createBackgroundNode() -> SKNode {
+//        // 1
+//        // Create the node
+//        let backgroundNode = SKNode()
+//        let ySpacing = 64.0 * scaleFactor
+//        
+//        // 2
+//        // Go through images until the entire background is built
+//        for index in 0...19 {
+//            //3
+//            let node = SKSpriteNode(imageNamed:String(format: "Background%02d", index + 1))
+//            //4
+//            node.setScale(scaleFactor)
+//            node.anchorPoint = CGPoint(x: 0.5, y: 0.0)
+//            node.position = CGPoint(x: self.size.width, y: ySpacing * CGFloat(index))
+//            //5
+//            backgroundNode.addChild(node)
+//        }
+//        
+//        //6
+//        //Return the completed background node
+//    }
+}
---
+++ Jump/GameViewController.swift
@@ -0,0 +1,49 @@
+//
+//  GameViewController.swift
+//  Jump
+//
+//  Created by Xiulan Shi on 11/21/15.
+//  Copyright (c) 2015 Xiulan Shi. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class GameViewController: UIViewController {
+
+    // First Step
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let skView = self.view as! SKView
+        
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+        
+        let scene = GameScene(size: skView.bounds.size)
+        scene.scaleMode = .AspectFit
+        
+        skView.presentScene(scene)
+    }
+    
+    override func shouldAutorotate() -> Bool {
+        return true
+    }
+
+    override func supportedInterfaceOrientations() -> UIInterfaceOrientationMask {
+        if UIDevice.currentDevice().userInterfaceIdiom == .Phone {
+            return .AllButUpsideDown
+        } else {
+            return .All
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Release any cached data, images, etc that aren't in use.
+    }
+
+    override func prefersStatusBarHidden() -> Bool {
+        return true
+    }
+}
