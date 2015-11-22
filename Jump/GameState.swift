97e266d1d46d9ec77f76668be80603c51aac6f2b
Xiulan Shi <xiulanshi2013@gmail.com>
2015-11-22 11:43:00 -0500

    Added the scoring system

---
+++ Jump/GameState.swift
@@ -0,0 +1,49 @@
+//
+//  GameState.swift
+//  Jump
+//
+//  Created by Xiulan Shi on 11/22/15.
+//  Copyright © 2015 Xiulan Shi. All rights reserved.
+//
+
+import Foundation
+
+class GameState {
+    
+    var score: Int
+    var highScore: Int
+    var stars: Int
+    
+    class var sharedInstance :GameState {
+        struct Singleton {
+            static let instance = GameState()
+        }
+        
+        return Singleton.instance
+    }
+    
+    init() {
+        // Init
+        score = 0
+        highScore = 0
+        stars = 0
+        
+        // Load game state
+        let defaults = NSUserDefaults.standardUserDefaults()
+        
+        highScore = defaults.integerForKey("highScore")
+        stars = defaults.integerForKey("stars")
+    }
+    
+    func saveState() {
+        // Update highScore if the current score is greater
+        highScore = max(score, highScore)
+        
+        // Store in user defaults
+        let defaults = NSUserDefaults.standardUserDefaults()
+        defaults.setInteger(highScore, forKey: "highScore")
+        defaults.setInteger(stars, forKey: "stars")
+        NSUserDefaults.standardUserDefaults().synchronize()
+    }
+    
+}
